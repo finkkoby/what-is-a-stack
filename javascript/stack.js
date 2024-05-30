bed3628653fff9b47fb378f132755047a6976a7a
Koby Fink <finkkoby@icloud.com>
2024-05-30 09:09:20 -0400

    complete lab

--- javascript/stack.js
+++ javascript/stack.js
@@ -9,43 +9,62 @@ class Stack {
   // add item to top of stack if not full
   // if full throw error
   push(item) {
-
+    if (this.isFull()) {
+      throw error
+    } else {
+      this.stack.push(item)
+    }
   }
 
   // remove item from top of stack and return it
   pop() {
-
+    return this.stack.pop()
   }
 
   // return item at top of stack without removing it
   peek() {
-
+    return this.stack[this.stack.length - 1]
   }
 
   // return true if stack is empty, otherwise false
   isEmpty() {
-
+    if (this.stack.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   // return true if stack is full, otherwise false
   isFull() {
-
+    if (this.stack.length === this.limit) {
+      return true
+    } else {
+      return false
+    }
   }
 
   // return number of items in stack
   size() {
-
+    return this.stack.length
   }
 
   // return -1 if item not in stack, otherwise integer representing 
   // how far it is from the top
   search(target) {
-
+    for (let i = 0; i < this.stack.length; i++) {
+      if (this.stack[i] === target) {
+        return this.stack.length - i - 1
+      }
+    }
+    return -1
   }
 
   // print contents of stack: do not return the stack itself!
   print() {
-    
+    for (let n of this.stack) {
+      console.log(n)
+    }
   }
 }
 
